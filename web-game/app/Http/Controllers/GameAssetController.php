da53a11c4f49fcc3646189e2ee0a151315b7015e
Novandi Kevin Pratama <kevinpret27@gmail.com>
2021-11-09 20:02:30 +0700

    Tambahan Laravel

---
+++ web-game/app/Http/Controllers/GameAssetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GameAsset;
+use Illuminate\Http\Request;
+
+class GameAssetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GameAsset  $gameAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GameAsset $gameAsset)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GameAsset  $gameAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GameAsset $gameAsset)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GameAsset  $gameAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GameAsset $gameAsset)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GameAsset  $gameAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GameAsset $gameAsset)
+    {
+        //
+    }
+}
